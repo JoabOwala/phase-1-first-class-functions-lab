7a018f0a9ebfc84728839fb860179a4610f0a041
Joab Owala <owalajoab@gmail.com>
2023-03-15 22:19:57 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+//Code your solution here
+
+const driverList = [];
+function returnFirstTwoDrivers(names){
+  for (let i = 0; i < names.length; i++){
+    driverList.push(`${names[i]}`);
+  }
+  return driverList.slice(0,2);
+}
+
+function returnLastTwoDrivers(names){
+    for (let i = 0; i < names.length; i++){
+        driverList.push(`${names[i]}`);
+      }
+    return driverList.slice(-2);
+}
+
+function selectDifferentDrivers(names,returnLastTwoDrivers){
+    return returnLastTwoDrivers();
+}
+
+
+function selectingDrivers(returnFirstTwoDrivers){
+    return returnFirstTwoDrivers();
+
+}
+
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function createFareMultiplier(value){
+    return function(){return value * value};
+}
